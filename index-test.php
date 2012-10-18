a424f6d625293b70c2da70452089e20c92ac5e05
user <user@rpz-1.np.ua>
2012-10-18 10:13:56 +0300

    fix

--- index-test.php
+++ index-test.php
@@ -5,11 +5,13 @@
  */
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../framework/yii.php';
+$yii=dirname(__FILE__).'/../yiiframework/yii.php';
 $config=dirname(__FILE__).'/protected/config/test.php';
 
 // remove the following line when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
+// specify how many levels of call stack should be shown in each log message
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 5);
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
